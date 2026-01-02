03e6c32b46c3b758515838bae161dce52685163d
maksympash <maksympash@gmail.com>
2026-01-02 18:09:41 +0200

    Fixed Fact.kt in domain layer

--- app/src/main/java/com/example/useraccountapp/domain/models/Fact.kt
+++ app/src/main/java/com/example/useraccountapp/domain/models/Fact.kt
@@ -1,10 +1,10 @@
 package com.example.useraccountapp.domain.models
 
 data class Fact(
-    val id: String?,
-    val text: String?,
+    val id: String,
+    val text: String,
     val source: String?,
     val sourceUrl: String?,
     val language: String?,
-    val permalink: String?,
+    val permalink: String,
 )
